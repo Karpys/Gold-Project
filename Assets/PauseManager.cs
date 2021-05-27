86d7f66d0cfd8f3845629c77fecab2913367c9eb
nicolas <nico.valadou@orange.fr>
2021-05-27 17:10:10 +0200

    Merge branch 'Nicolas' into main

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [Header("Mask")]
+    public GameObject pauseMask;
+    public GameObject[] target;
+
+    [Header("Bouton")]
+    public GameObject[] bouton;
+    public GameObject[] targetBouton;
+
+   [Header("AudioListener")]
+    public AudioSource sound;
+
+    [Header("Name Scene")]
+    public string nameSceneRetry;
+    public string nameSceneMenu;
+
+    [Header("")]
+    public float speed;
+
+    private bool open = false;
+    private bool close = true;
+    private float lerp = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pauseMask.transform.position.x < targetBouton[0].transform.position.x)
+            bouton[0].SetActive(true);
+        else
+            bouton[0].SetActive(false);
+
+        if (pauseMask.transform.position.x < targetBouton[1].transform.position.x)
+            bouton[1].SetActive(true);
+        else
+            bouton[1].SetActive(false);
+
+        if (pauseMask.transform.position.x < target[1].transform.position.x + 0.5)
+            bouton[2].SetActive(true);
+        else
+            bouton[2].SetActive(false);
+
+        if (open)
+        {
+            lerp += Time.deltaTime * speed;
+
+            if(lerp<1)
+                pauseMask.transform.position = Vector3.Lerp(pauseMask.transform.position, target[1].transform.position, lerp);
+            else
+                lerp = 0;
+
+        }
+
+        if (close)
+        {
+            lerp += Time.deltaTime * speed;
+
+            if (pauseMask.transform.position.x!= target[0].transform.position.x)
+                pauseMask.transform.position = Vector3.Lerp(pauseMask.transform.position, target[0].transform.position, lerp);
+            else
+                lerp = 0;
+
+        }
+    }
+
+    public void OpenPause()
+    {
+        if(!open)
+        {
+            open = true;
+            close = false;
+            lerp = 0;
+        }
+        else
+        {
+            close = true;
+            open = false;
+            lerp = 0;
+        }
+
+    }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene(nameSceneMenu);
+    }
+
+    public void Sound(bool onOff)
+    {
+        if(!onOff)
+            sound.volume = 0;
+        else
+            sound.volume = 1;
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(nameSceneRetry);
+    }
+}
