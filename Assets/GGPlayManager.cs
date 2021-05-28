ddfdefa92de5bef013994a6d861594064014545b
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-05-28 11:39:51 +0200

    [Fix] Try Fix GGManager

--- Assets/GGPlayManager.cs
+++ Assets/GGPlayManager.cs
@@ -15,17 +15,7 @@ public class GGPlayManager : MonoBehaviour
     private void Awake()
     {
         PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
-        // enables saving game progress.
-        .EnableSavedGames()
-        // requests the email address of the player be available.
-        // Will bring up a prompt for consent.
-        .RequestEmail()
-        // requests a server auth code be generated so it can be passed to an
-        //  associated back end server application and exchanged for an OAuth token.
-        .RequestServerAuthCode(false)
-        // requests an ID token be generated.  This OAuth token can be used to
-        //  identify the player to other services such as Firebase.
-        .RequestIdToken()
+        
         .Build();
 
         PlayGamesPlatform.InitializeInstance(config);
