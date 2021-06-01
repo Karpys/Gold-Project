0ccd9b2344c4877d7783ac7dd10516121d21ee29
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-06-01 16:15:23 +0200

    [Feat/Fix] Fix Camera Bug and Trier la peste Assets

--- Assets/Scripts/SetResolution.cs
+++ Assets/Scripts/SetResolution.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class SetResolution : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float BaseRatio = 16f / 9f;
-    public float RatioApply;
-    public float Width;
-    public float Height;
     
+    public float BaseRatio = 16f / 9f;
+    private void Awake()
+    {
+
+        //DontDestroyOnLoad(this);
+    }
     void Start()
     {
         Camera cam = GetComponent<Camera>();
-        Width = Screen.width;
-        Height = Screen.height;
-        RatioApply = Width / Height;
-        cam.orthographicSize = cam.orthographicSize * BaseRatio / RatioApply;
+        cam.orthographicSize = cam.orthographicSize * BaseRatio / BaseResolution.Resolution.RatioApplied;
     }
 
  
 }
+
